ba521648bb51a5739e79c66951a083d3988fcb5c
Abbas <AbbasHussainSyed@users.noreply.github.com>
2024-11-15 14:03:10 -0800

    Auto-commit JS on save for 227

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -3,10 +3,14 @@ import resumePDF from '../assets/AbbasSyedSDEFV1.0.pdf';
 
 function Resume() {
     return (
-        <section className="resume">
-            <h2>My Resume</h2>
-            <iframe src={resumePDF} title="Resume" width="100%" height="600px"></iframe>
-            <a href={resumePDF} download>Download Resume</a>
+        <section className="resume py-10 sm:py-20 bg-gray-200 px-4">
+            <div className="container mx-auto text-center">
+                <h2 className="text-3xl sm:text-4xl font-semibold mb-4 sm:mb-6">My Resume</h2>
+                <iframe className="w-full h-80 sm:h-96 mb-4" src={resumePDF} title="Resume"></iframe>
+                <a href={resumePDF} className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition" download>
+                    Download Resume
+                </a>
+            </div>
         </section>
     );
 }
